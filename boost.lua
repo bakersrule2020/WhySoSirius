3d659e1d740dc18aea6c5af1624363d10cdc990a
Jenson Hirst <shlex@sirius.menu>
2024-11-22 18:55:44 +0000

    Update boost.lua

--- boost.lua
+++ boost.lua
@@ -1,36 +1,69 @@
 
--- Boosters
-local boosts = {
-	-- Example: [304343782] = {icon = 1, color = Color3.fromRGB(255, 255, 255)},
-  -- Example 2: 304343782,
-  
-	304343782, -- 837791888643588136
-	65435157, -- 1254282925550207050
-	7239379014, -- 1178865930609115237
-	186210626 -- 735162090880237619
-}
+-- Sirius Boosts
+-- sirius.menu/privacy | sirius.menu/terms
 
+-- Request
+local request = (http and http.request) or http_request or request or HttpPost
 
-
-
--- Code
+-- Services
+local httpService = game:GetService('HttpService')
 local players = game:GetService('Players')
-local coreGui = game:GetService('CoreGui')
+local coreGui = game:GetService('RunService'):IsStudio() and script.Parent or game:GetService('CoreGui')
 local userInputService = game:GetService("UserInputService")
+
+-- GET Boosts
+local response
+
+if not request then 
+	response = httpService:GetAsync('https://sync.sirius.menu/v1/u') 
+else
+	response = request({
+		Url = 'https://sync.sirius.menu/v1/u',
+		Method = "GET",
+	}).Body
+end
+
+local success, boosts = pcall(function() return httpService:JSONDecode(response) end)
+
+local function getBooster(userId)
+	userId = tostring(userId) -- ensure string to match json
+	local properties
+	
+	for id, prop in pairs(boosts) do
+		if id == userId then
+			properties = prop
+			break
+		end
+	end
+
+	if properties then
+		local booster = {}
+
+		if properties.color and not (properties.color[1] > 255 or properties.color[2] > 255 or properties.color[3] > 255) then -- Color higher than 255 means default color value (no changes made)
+			booster.color = Color3.fromRGB(properties.color[1], properties.color[2], properties.color[3])
+		end
+
+		booster.icon = properties.icon ~= 0 and properties.icon or nil -- Icon 0 means default icon (no changes made)
+		
+		return booster
+	else
+		return false
+	end
+end
 
 local function findOverlayFrame(target)
 	if not target then return nil end
 	local frame = target:FindFirstChild("ChildrenFrame")
-	
+
 	if frame then
 		local nameFrame = frame:FindFirstChild("NameFrame")
-		
+
 		if nameFrame then
 			if userInputService.TouchEnabled then
 				return nameFrame
 			else
 				local bgFrame = nameFrame:FindFirstChild("BGFrame")
-				
+
 				if bgFrame then
 					return bgFrame:FindFirstChild("OverlayFrame")
 				end
@@ -41,8 +74,8 @@ local function findOverlayFrame(target)
 end
 
 local function display(userId, booster)
-	local target = coreGui:FindFirstChild("p_" .. userId, true) or coreGui:FindFirstChild("Player_" .. userId, true)
-	if not target then return end
+	local target = coreGui:FindFirstChild("p_" .. tostring(userId), true) or coreGui:FindFirstChild("Player_" .. tostring(userId), true)
+	if not target or not booster then return end
 
 	local overlayFrame = findOverlayFrame(target)
 
@@ -52,28 +85,15 @@ local function display(userId, booster)
 		overlayFrame.PlayerIcon.ImageRectSize = Vector2.zero
 		if userInputService.TouchEnabled then
 			overlayFrame.PlayerName.TextColor3 = booster and booster.color or Color3.fromRGB(255, 138, 250)
-			overlayFrame.PlayerName.TextTransparency = 0
 		else
 			overlayFrame.PlayerName.PlayerName.TextColor3 = booster and booster.color or Color3.fromRGB(255, 138, 250)
-			overlayFrame.PlayerName.PlayerName.TextTransparency = 0
 		end
-		
 	end
 end
 
 local function processPlayer(player)
-	local booster = boosts[player.UserId]
-	
-	if booster then
-		display(player.UserId, typeof(booster) == "table" and booster or nil)
-	else
-		for _, id in pairs(boosts) do
-			if id == player.UserId then
-				display(player.UserId, nil)
-				break
-			end
-		end
-	end
+	local booster = getBooster(player.UserId)
+	display(player.UserId, booster)
 end
 
 local function processAllPlayers()
@@ -83,7 +103,6 @@ local function processAllPlayers()
 end
 
 processAllPlayers()
-
 players.PlayerAdded:Connect(processPlayer)
 
 if userInputService.TouchEnabled then
